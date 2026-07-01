48df56078e328c225c086a0e72d54eabec0cbaf7
buyan-kh <vicekurel@gmail.com>
2026-07-01 12:00:14 -0700

    chore: scaffold mint ios project

---
+++ Mint/Views/RootView.swift
@@ -0,0 +1,13 @@
+import SwiftUI
+
+struct RootView: View {
+    var body: some View {
+        Text("Mint")
+            .font(.system(size: 26, weight: .bold))
+            .foregroundStyle(Color(red: 0.067, green: 0.067, blue: 0.067))
+    }
+}
+
+#Preview {
+    RootView()
+}
